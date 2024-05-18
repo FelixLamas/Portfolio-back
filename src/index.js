cbd5f0eac509ae7459b727182ebaa227abc8fcb3
Felix ELoy Lamas <felamas@gmail.com>
2024-05-17 21:39:36 -0300

    se creo estructura basica, se realizo conexion a la bd y se creo archivo index.html

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const path = require("path");
+const dataBaseC = require("./dbConection.js");
+
+const app = express();
+
+require("dotenv").config();
+dataBaseC();
+app.set("port", process.env.PORT || 5000);
+app.listen(app.get("port"), () => {
+  console.log(
+    "Backend del Portfolio escuchando en el puerto: ",
+    app.get("port")
+  );
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+//ruta de prueba para el back
+app.get("/test", async (req, res) => {
+  try {
+    return res.status(200).json({ message: "realizo la consulta ok" });
+  } catch (error) {
+    console.error(error);
+  }
+});
